a1dd8f5f3901197a3a35a45176eb180e18936dc1
Thanushan1523 <thanushan1523@gmail.com>
2025-01-11 07:24:21 -0500

    PAGES CREATED FOR ROUTING

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Card from './pages/Cart'
+import Login from './pages/Login'
+import Order from './pages/Order'
+import PlaceOrder from './pages/PlaceOrder'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>Apphj  gf</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product/:productId' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/order' element={<Order/>} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assests'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={assets.logo} className='w-36'/>
+    </div>
+  )
+}
+
+export default Navbar
