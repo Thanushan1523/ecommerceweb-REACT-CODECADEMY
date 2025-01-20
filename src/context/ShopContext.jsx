c241819be74bffb45c6383ca168f96416494e607
Thanushan1523 <thanushan1523@gmail.com>
2025-01-20 08:23:48 -0500

    developing shopcont component

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,3 +1,13 @@
 import { createContext } from "react";
-
+import { products } from "../assets/assests";
 export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+  const value = {};
+  return (
+    <ShopContext.Provider value={value}>
+        {props.children}
+        </ShopContext.Provider>
+  );
+};
+export default ShopContextProvider;
