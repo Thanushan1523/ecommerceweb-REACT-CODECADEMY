d2c36bc3f59d0edc323a1f93a1a9b85cc3a10388
Thanushan1523 <thanushan1523@gmail.com>
2025-01-24 08:35:41 -0500

    working on latesd products collection

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -6,8 +6,12 @@ const LatestCollection = () => {
  const {products}= useContext(ShopContext);
  
     return (
-    <div>
-      <Title text1={Latestd}/>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+      <Title text1={'LATEST'} text2={'COLLECTION'}/>
+      <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>I doing turing for last 4 years . Math & Chemistry </p>
+    </div>
+      
     </div>
   )
 }
