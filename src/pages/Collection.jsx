6b508fd450d75f96bef534f9b157770f15fa95c7
Thanushan1523 <thanushan1523@gmail.com>
2025-02-02 11:52:36 -0500

    DEVELOPING THE COLLECTION PAGE

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-
+import React ,{useContext, useState} from 'react'
+import { ShopContext } from '../context/ShopContext'
 const Collection = () => {
+  const {products}= useContext(ShopContext);
+  const [showFilter,setShowFilter]=useState(false);
   return (
-    <div>Collection</div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 border-t '>
+      {/* {filter options} */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2 '>FILTERS</p>
+      {/* {category filter } */}
+      <div className={`border border-gray-500 pl-5 py-3 mt-6 sm:block ${showFilter ? '' :'hidden' } `}>
+      <p className='mb-3 text-sm font-medium '>Category</p>
+      <div className='flex flex-col gap-2 text-sm font-light text-gray-700' >
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Men'}/> Men
+        </p>
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Women'}/> Women
+        </p>
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Kids'}/> Kids
+        </p>
+      </div>
+      </div>
+      {/* {subcategory filter } */}
+      <div className={`border border-gray-500 pl-5 py-3 mt-6 sm:block ${showFilter ? '' :'hidden' } `}>
+      <p className='mb-3 text-sm font-medium '>Type</p>
+      <div className='flex flex-col gap-2 text-sm font-light text-gray-700' >
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Topwear'}/> Topwear
+        </p>
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Bottomwear'}/> Bottomwear
+        </p>
+        <p className='flex gap-2'>
+          <input className='w-3' type='checkbox' value={'Winterwear'}/> Winterwear
+        </p>
+      </div>
+      </div>
+      </div>
+
+    </div>
   )
 }
 
