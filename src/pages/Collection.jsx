e2b26d5cd21f6e208f6648b3aed4c7e20644e6bd
Thanushan1523 <thanushan1523@gmail.com>
2025-02-03 21:25:39 -0500

    DEVELOPING THE COLLECTION PAGE

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,13 +1,14 @@
 import React ,{useContext, useState} from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assests';
 const Collection = () => {
   const {products}= useContext(ShopContext);
   const [showFilter,setShowFilter]=useState(false);
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 border-t '>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t '> 
       {/* {filter options} */}
       <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2 '>FILTERS</p>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2 '>FILTERS <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90':'' }`} src={assets.dropdown_icon} alt='no dropdown image'/></p>
       {/* {category filter } */}
       <div className={`border border-gray-500 pl-5 py-3 mt-6 sm:block ${showFilter ? '' :'hidden' } `}>
       <p className='mb-3 text-sm font-medium '>Category</p>
@@ -24,7 +25,7 @@ const Collection = () => {
       </div>
       </div>
       {/* {subcategory filter } */}
-      <div className={`border border-gray-500 pl-5 py-3 mt-6 sm:block ${showFilter ? '' :'hidden' } `}>
+      <div className={`border border-gray-500 pl-5 py-3 my-5 sm:block  ${showFilter ? '' :'hidden' } `}>
       <p className='mb-3 text-sm font-medium '>Type</p>
       <div className='flex flex-col gap-2 text-sm font-light text-gray-700' >
         <p className='flex gap-2'>
