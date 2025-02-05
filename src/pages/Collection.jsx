909ccdf76a5bfbed206a2f38dde8b841530aee2b
Thanushan1523 <thanushan1523@gmail.com>
2025-02-05 08:16:43 -0500

    DEVELOPING THE COLLECTION PAGE -right side components

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,6 +1,10 @@
 import React ,{useContext, useState} from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assests';
+import Title from '../components/Title';
+
+
+
 const Collection = () => {
   const {products}= useContext(ShopContext);
   const [showFilter,setShowFilter]=useState(false);
@@ -8,7 +12,8 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t '> 
       {/* {filter options} */}
       <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2 '>FILTERS <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90':'' }`} src={assets.dropdown_icon} alt='no dropdown image'/></p>
+        <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2 '>FILTERS 
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90':'' }`} src={assets.dropdown_icon} alt='no dropdown image'/></p>
       {/* {category filter } */}
       <div className={`border border-gray-500 pl-5 py-3 mt-6 sm:block ${showFilter ? '' :'hidden' } `}>
       <p className='mb-3 text-sm font-medium '>Category</p>
@@ -40,7 +45,12 @@ const Collection = () => {
       </div>
       </div>
       </div>
-
+{/* {right side } */}
+<div className='flex-1'>
+  <div className='flex justify-between text-base sm:text-2xl mb-4'>
+        <Title text1={'ALL'} text2={'COLLECTIONS'}/>
+  </div>
+</div>
     </div>
   )
 }
