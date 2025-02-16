ab804b3d8d57056713c96ee9df837aaf68cc9a00
Thanushan1523 <thanushan1523@gmail.com>
2025-02-16 10:46:21 -0500

    DEVELOPING THE COLLECTION PAGE -Search bar.jsx created

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,12 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assests";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency ='$';
     const delivery_fee =10;
+    const[search,setSearch]=useState('');
+    const[showSerch, setShowSearch] =useState(true)
+
+
   const value = {
-    products , currency,delivery_fee
+    products , currency,delivery_fee,
+    search,setSearch,showSerch,setSearch
   };
   return (
     <ShopContext.Provider value={value}>
---
+++ src/pages/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useContext } from 'react'
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
