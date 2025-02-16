77f43023f0b4623eec5fbdab606a3a2714de5a92
Thanushan1523 <thanushan1523@gmail.com>
2025-02-16 12:40:13 -0500

    DEVELOPING THE COLLECTION PAGE -Search option link with navbar search option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assests'
 import {NavLink ,Link} from 'react-router'
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
   const[visible,setVisible] =useState(false);
+  const {setShowSearch }= useContext(ShopContext)
+
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
        <Link to='/'> <img src={assets.logo} className='w-20'/></Link>
@@ -33,7 +37,7 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-6'>
-          <img src={assets.search_icon} className='w-5 cursor-pointer' alt=''/>
+          <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt=''/>
           
           <div className='group relative'>
             <img className='w-5 cursor-pointer' src={assets.profile_icon} alt=""/>
