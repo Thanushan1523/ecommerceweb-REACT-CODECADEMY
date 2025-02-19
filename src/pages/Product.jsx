fde3d4e237ba59eb35f6fc5f5a900e36841019cf
Thanushan1523 <thanushan1523@gmail.com>
2025-02-18 20:23:18 -0500

    DEVELOPING THE COLLECTION PAGE -logic added -when we type something in search bar its showing the reasults

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+const {productId}=useParams();
+const {products} =useContext(ShopContext);
+const[productData,setProductData]= useState(false);
+ 
+const fetchProductData = async () => {
+  products.map((item) =>{
+    if (item._id === productId){
+      setProductData(item)
+      console.log(item);
+      return null;
+    }
+  })
+}
   return (
-    <div>Product</div>
+    <div>
+
+    </div>
   )
 }
 
