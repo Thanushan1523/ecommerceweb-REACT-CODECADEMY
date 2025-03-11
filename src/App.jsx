2c82c2acf18bcab5d674a14b66abedbd0cf73346
Thanushan1523 <thanushan1523@gmail.com>
2025-03-11 13:39:04 -0400

    DEVELOPING THE PRODUCT PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Cart from './pages/Cart'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './pages/SearchBar'
+import Product from './pages/Product'
 
 const App = () => {
   return (
@@ -23,7 +24,7 @@ const App = () => {
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
-        <Route path='/product/:productId' element={<Home/>} />
+        <Route path='/product/:productId' element={<Product/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/order' element={<Order/>} />
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,11 +7,12 @@ const Product = () => {
 const {productId}=useParams();
 const {products} =useContext(ShopContext);
 const[productData,setProductData]= useState(false);
- 
+const[image,setImage]=useState(' ')
 const fetchProductData = async () => {
   products.find((item) =>{
-    if (item._id === productId){
+    if (item._id === productId) {
       setProductData(item)
+      setImage(item.image[0])
       console.log(item);
       return null;
     }
@@ -20,11 +21,23 @@ const fetchProductData = async () => {
 useEffect(()=>{
   fetchProductData();
 },[productId ,products])
-  return (
-    <div>
-
+  return productData ?(
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* product data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/* product image */}
+        <div className='flex-1 flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item,index)=>(
+                <img src={item} key={index} className=' w-[24%'/>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  ):<div className='opacity-0'></div> 
 }
 
 export default Product
