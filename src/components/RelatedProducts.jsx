13bfc878fff02e14975d29cdbe85937885d6a796
Thanushan1523 <thanushan1523@gmail.com>
2025-04-01 07:27:13 -0400

    DEVELOPING THE PRODUCT PAGE-adding related products so developing seperate jsx file

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,29 +1,36 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-
+import { useEffect } from 'react';
 
 const RelatedProducts = ({category,subCategory}) => {
   const {products} =useContext(ShopContext);
   const [related,setRelated] =useState([]);
 
   useEffect(() =>{
-    if(products.length > 0){
+    if(products.length > 0 )
+      {
       let productsCopy =products.slice();
       productsCopy =productsCopy.filter((item)=> category === item.category);
       productsCopy = productsCopy.filter((item)=subCategory === item.subCategory)
       
-      console.log(productsCopy.slice(0,5));
+      setRelated(productsCopy.slice(0,5));
+     
     }
 
 
-  } ,[products])
+  } ,[products ])
   
   
   return (
     <div>
-        
+         <h2>Related Products</h2>
+      <ul>
+        {related.map(item => (
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default RelatedProducts
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assests';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   
@@ -86,7 +87,7 @@ useEffect(()=>{
       </div>
 
       {/* ...............related products............... */}
-        
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ):<div className='opacity-0'></div> 
 }
