9835ebebfc6bacb17a2467c8794b3d7f39a5e6ac
Thanushan1523 <thanushan1523@gmail.com>
2025-04-03 07:45:17 -0400

    DEVELOPING THE PRODUCT PAGE-adding addtocart function

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,7 +7,26 @@ const ShopContextProvider = (props) => {
     const delivery_fee =10;
     const[search,setSearch]=useState('');
     const[showSearch, setShowSearch] =useState(false)
+    const[cartItems ,setCartItems]=useState({});
 
+  const addToCart =async (itemId,size)=>{
+    let cartData= structuredClone(cartItems);
+     if(cartData[itemId]){
+      if (cartData[itemId][size]){
+        cartData[itemId][size] +=1;
+
+      }
+      else{
+        cartData[itemId][size]=1;
+      }
+     }
+     else{
+        cartData[itemId]={};
+        cartData[itemId][size] =1;
+      
+     }
+     setCartItems(cartData)
+  } 
 
   const value = {
     products , currency,delivery_fee,
