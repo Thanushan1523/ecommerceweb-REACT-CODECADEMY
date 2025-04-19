a690d08bb916fccb7b749ccea90431c178d9915f
Thanushan1523 <thanushan1523@gmail.com>
2025-04-19 19:57:37 -0400

    DEVELOPING THE cart page -carttotal function created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from '../components/Title';
 import { assets } from '../assets/assests';
+import CartTotal from '../components/CartTotal';
 
 
 const Cart = () => {
@@ -52,6 +53,11 @@ const Cart = () => {
             )
           })
         }
+      </div>
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+        <CartTotal/>
+        </div>
       </div>
     </div>
   )
