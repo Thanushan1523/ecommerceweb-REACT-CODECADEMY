469bbdc8624cf0865696cb53dd1ec019b7fd3bb6
Thanushan1523 <thanushan1523@gmail.com>
2025-04-26 17:53:59 -0400

    DEVELOPING place order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Title from '../components/Title';
+import CartTotal from '../components/CartTotal';
+import { assets } from '../assets/assests';
 
 const PlaceOrder = () => {
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       {/* Left side */}
-      <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+      <div className='flex flex-col gap-4 w-full sm:max-w-[480px] bg-orange-400'>
         <div className='text-xl sm:text-2xl my-3'>
           <Title text1={'DELIVERY'} text2={'INFORMATION'} />
         </div>
@@ -28,6 +30,26 @@ const PlaceOrder = () => {
         
         </div>
         <input  className='border border-gray-300 py-1.5 px-3.5 w-full' type='number' placeholder='Phone Number ' />
+        </div>
+
+
+        {/* right dise */}
+
+
+        <div className='mt-8'>
+          <div className='mt-8 min-w-80 '>
+            <CartTotal/>
+          </div>
+          <div className='mt-12'>
+            <Title text1={'PAYMENT'} text2={'METHOD'} />
+            <div className=' flex gap-3 flex-col lg:flex-row'>
+              <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <img className='h-5 mx-4' src={assets.stripe_logo} /> 
+
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
