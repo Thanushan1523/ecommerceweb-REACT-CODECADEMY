ed3d36f092f3d74a2a6fca5cefe676adf37ee153
Thanushan1523 <thanushan1523@gmail.com>
2025-04-29 07:58:09 -0400

    DEVELOPING ORDER PAGE

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -16,7 +16,19 @@ const Order = () => {
           <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-center md:justify-between gap-4'>
 
             <div className='flex items-start gap-6 text-sm'>
+              <img  className='w-16 sm:w-20'  src={item.image[0]} alt=''/>
+              <div>
+                <p  className='sm:text-base font-medium'>{item.name}</p>
+                <div>
+                  <p className='text-lg'>{currency}{item.price}</p>
+                  <p>Quantity:1</p>
+                  <p>Size :m </p>
+                  </div>
+                  <p>Date<span className='text-gray-400'>25,july</span></p>
+                </div>
+
               </div>
+
 
             </div>
 
