bb92330d227bb5156203760357d7a7ca171e2b9f
Thanushan1523 <thanushan1523@gmail.com>
2025-05-06 18:30:11 -0400

    DEVELOPING LOGIN  PAGE-dinamic login & signup page is created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Title from '../components/Title';
+import { assets } from '../assets/assests';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+      <Title text1={'YOUR'}  text2={'CART'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16' >
+              <img className='w-full md:max-w-[450px]' src={assets.about_img} alt=" " />
+              <div>
+                
+              </div>
+      </div>
+    </div>
   )
 }
 
